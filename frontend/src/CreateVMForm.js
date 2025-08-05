f8b0e6c7c560991b3f6c55638b5728483054e346
bazigan <bazigannetwork@gmail.com>
2025-08-05 23:06:29 +0700

    refactor: restructure application by adding App and CreateVMForm components

--- frontend/src/CreateVMForm.js
+++ frontend/src/CreateVMForm.js
@@ -25,7 +25,7 @@ export default function CreateVMForm() {
     setLoading(true);
     setMessage('');
     try {
-      const res = await fetch('http://localhost:5000/api/create-vm', {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/create-vm`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
